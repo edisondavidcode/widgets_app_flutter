500b87d9c395227cb99d1ed11d0d6b5fb2becabb
EdisonDavidCode <edisondavidr.rd@gmail.com>
2023-12-08 21:14:54 -0400

    go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app_flutter/config/router/app_router.dart';
 import 'package:widgets_app_flutter/config/theme/app_theme.dart';
-import 'package:widgets_app_flutter/presentation/screens/buttons/buttons_screen.dart';
-import 'package:widgets_app_flutter/presentation/screens/cards/cards_screen.dart';
-import 'package:widgets_app_flutter/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,14 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 0).getTheme(),
-      home: const HomeScreen(),
-      routes: {
-        '/buttons': (context) => const ButtonsScreen(),
-        '/cards': (context) => const CardScreen(),
-      },
     );
   }
 }
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'package:widgets_app_flutter/presentation/screens/buttons/buttons_screen.dart';
+export 'package:widgets_app_flutter/presentation/screens/cards/cards_screen.dart';
+export 'package:widgets_app_flutter/presentation/screens/home/home_screen.dart';
