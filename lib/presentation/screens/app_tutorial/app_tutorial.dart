5460a86c8b326962d68356f449f5961c39da5870
EdisonDavidCode <edisondavidr.rd@gmail.com>
2023-12-25 18:11:13 -0400

    Crear todas las pantallas faltantes

---
+++ lib/presentation/screens/app_tutorial/app_tutorial.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTutorial extends StatelessWidget {
+  static const name = 'app_tutorial';
+
+  const AppTutorial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
