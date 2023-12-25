56b638666dc50a31c8c6c0c5abd4aea12a1b2cf0
EdisonDavidCode <edisondavidr.rd@gmail.com>
2023-12-25 14:33:57 -0400

    Botón personalizado

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -67,10 +67,34 @@ class _ButtonsView extends StatelessWidget {
               backgroundColor: MaterialStatePropertyAll(colors.primary),
               iconColor: const MaterialStatePropertyAll(Colors.white),
             ),
-          )
-
-          // TODO: CUSTOM BUTTON
+          ),
+          const CustomButton()
         ]),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                'Hola mundo',
+                style: TextStyle(color: Colors.white),
+              )),
+        ),
       ),
     );
   }
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app_flutter/config/menu/menu_items.dart';
-import 'package:widgets_app_flutter/presentation/screens/screens.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
