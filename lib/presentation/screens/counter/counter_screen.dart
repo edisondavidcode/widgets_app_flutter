f69f12aef58917ab44f98f3fa1d33b134f3dde31
EdisonDavidCode <edisondavidr.rd@gmail.com>
2024-01-06 20:56:54 -0400

    Cambiar valores y modificadores

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app_flutter/presentation/providers/counter_provider.dart';
+import 'package:widgets_app_flutter/presentation/providers/theme_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   const CounterScreen({super.key});
   static const name = 'counter_screen';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkmodeProvider);
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Counter Screen')),
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+            icon: isDarkMode
+                ? const Icon(Icons.dark_mode_outlined)
+                : const Icon(Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkmodeProvider.notifier).state = !isDarkMode;
+            },
+          )
+        ],
+      ),
       body: Center(
-        child:
-            Text('Valor : 10', style: Theme.of(context).textTheme.titleLarge),
+        child: Text('Valor : $clickCounter ',
+            style: Theme.of(context).textTheme.titleLarge),
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: 10),
         child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            ref.read(counterProvider.notifier).state++;
+          },
           child: const Icon(Icons.add),
         ),
       ),
